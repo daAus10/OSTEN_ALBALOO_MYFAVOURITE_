0f2caceaf436174a49c3170840ea2f8e9d15effd
amillerman <amillerman01@stclaircollege.ca>
2022-04-04 12:46:29 -0400

    Added routing rules for the remaining components and added links to them on the app component

--- learning-angular/src/app/app-routing.module.ts
+++ learning-angular/src/app/app-routing.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentDetailComponent } from './content-detail/content-detail.component';
 import { ContentListComponent } from './content-list/content-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
   {
+    path: "",
+    redirectTo: "/content",
+    pathMatch: "full"
+  },
+  {
     path: "content",
-    component: ContentListComponent,
-  }
+    component: ContentListComponent
+  },
+  {
+    path: "content/:id",
+    component: ContentDetailComponent
+  },
+  { path: "**", component: PageNotFoundComponent }
 ];
 @NgModule({
   imports: [
